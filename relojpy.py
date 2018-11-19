3e9c214b843b2f54d1088b39439a0a511ba176f1
Martín López Paglione <martincholp@hotmail.com>
2018-11-19 13:46:41 -0300

    Agregado el modo de aplicacion independiente

--- relojpy.py
+++ relojpy.py
@@ -7,47 +7,52 @@ from pygame.locals import *
 def apagar():
     pygame.quit()
     sys.exit()
-	
+    
 def eventos():
     for evento in pygame.event.get():
         if evento.type == QUIT:
             apagar()
 
 class Segmento(pygame.Surface):
-	"""Segmento para formar el caracter"""
-	def __init__(self, rect):
-		self.pos = (rect[0], rect[1])
+    """Segmento para formar el caracter"""
+    def __init__(self, rect):
+        self.pos = (rect[0], rect[1])
 
-		# verifico cual es el ancho y cual el largo
-		if rect[2] < rect[3]:
-			self.orientacion = "V"
-			self.largo = rect[3]
-			self.ancho = rect[2]
-	        self.ptos = [(0 ,self.ancho/2),(self.ancho/2 ,0),(self.ancho ,self.ancho/2),(self.ancho ,self.largo-self.ancho/2),(self.ancho/2 ,self.largo),(0,self.largo-self.ancho/2)]
-		else:
-			self.orientacion = "H"
-			self.largo = rect[2]
-			self.ancho = rect[3]
-	        self.ptos = [(0 ,self.ancho/2),(self.ancho/2 ,0),(self.largo-self.ancho/2 ,0),(self.largo ,self.ancho/2),(self.largo-self.ancho/2 ,self.ancho),(self.ancho/2,self.ancho)]
+        # verifico cual es el ancho y cual el largo
+        if rect[2] < rect[3]:
+            self.orientacion = "V"
+            self.largo = rect[3]
+            self.ancho = rect[2]
+            self.ptos = [(0              ,self.ancho/2),
+                         (self.ancho/2   ,0),
+                         (self.ancho     ,self.ancho/2),
+                         (self.ancho     ,self.largo-self.ancho/2),
+                         (self.ancho/2   ,self.largo),
+                         (0              ,self.largo-self.ancho/2)]
+        else:
+            self.orientacion = "H"
+            self.largo = rect[2]
+            self.ancho = rect[3]
+            self.ptos = [(0 ,self.ancho/2),(self.ancho/2 ,0),(self.largo-self.ancho/2 ,0),(self.largo ,self.ancho/2),(self.largo-self.ancho/2 ,self.ancho),(self.ancho/2,self.ancho)]
 
-		self.borde = 0  # Con 0 hago un segmento relleno
-		self.color = pygame.Color(255,255,255)  # Blanco por defecto
-		self.fondo = pygame.Color(0,0,0,0)  # Transparente por defecto
-		self.visible = True
+        self.borde = 0  # Con 0 hago un segmento relleno
+        self.color = pygame.Color(255,255,255)  # Blanco por defecto
+        self.fondo = pygame.Color(0,0,0,0)  # Transparente por defecto
+        self.visible = True
 
-		super(Segmento, self).__init__((rect[2]+1, rect[3]+1), pygame.HWSURFACE|pygame.SRCALPHA)
+        super(Segmento, self).__init__((rect[2]+1, rect[3]+1), pygame.HWSURFACE|pygame.SRCALPHA)
 
     def update(self):
         self.fill(self.fondo)
         pygame.draw.polygon(self, self.color, self.ptos, self.borde)
 
     def render(self, sup):
-    	if self.visible:
-	        sup.blit(self, self.pos)
+        if self.visible:
+            sup.blit(self, self.pos)
 
-		
-class Puntos(pygame.Surfce):
-	"""Puntos de separacion entre horas y minutos y entre minutos y segundos"""
+        
+class Puntos(pygame.Surface):
+    """Puntos de separacion entre horas y minutos y entre minutos y segundos"""
 
     def __init__(self, pos, r=5, e=20, a=122):
         '''pos es la posicion, r el radio, e es la separacion entre los puntos, a es la altura del caracter'''
@@ -59,33 +64,33 @@ class Puntos(pygame.Surfce):
         self.fondo = pygame.Color(0,0,0,0)  # Transparente por defecto
         self.visible = True
 
-    	super(Puntos, self).__init__((2*r, a), pygame.HWSURFACE|pygame.SRCALPHA)
+        super(Puntos, self).__init__((2*r, a), pygame.HWSURFACE|pygame.SRCALPHA)
 
     def update(self ):
-        self.fill(pygame.Color(50,0,0))
+        self.fill(self.fondo)
         pygame.draw.circle(self, self.color, (self.r,self.a/2-self.e/2), self.r, 0)
         pygame.draw.circle(self, self.color, (self.r,self.a/2+self.e/2), self.r, 0)
 
     def render(self, val, sup):
         if self.visible:
-        	if val==':':
-            	sup.blit(self, self.pos)
+            if val==':':
+                sup.blit(self, self.pos)
 
 class Caracter(pygame.Surface):
     '''Caracter de 7 segmentos'''
 
-	digitos = {
-	           '0':[True, True, True, True, True, True, False],
-	       	   '1':[False, False, True, True, False, False, False],
-	           '2':[False, True, True, False, True, True, True],
-	           '3':[False, True, True, True, True, False, True],
-	           '4':[True, False, True, True, False, False, True],
-	           '5':[True, True, False, True, True, False, True],
-	           '6':[True, True, False, True, True, True, True],
-	           '7':[False, True, True, True, False, False, False],
-	           '8':[True, True, True, True, True, True, True],
-	           '9':[True, True, True, True, True, False, True]
-		      }
+    digitos = {
+               '0':[True, True, True, True, True, True, False],
+               '1':[False, False, True, True, False, False, False],
+               '2':[False, True, True, False, True, True, True],
+               '3':[False, True, True, True, True, False, True],
+               '4':[True, False, True, True, False, False, True],
+               '5':[True, True, False, True, True, False, True],
+               '6':[True, True, False, True, True, True, True],
+               '7':[False, True, True, True, False, False, False],
+               '8':[True, True, True, True, True, True, True],
+               '9':[True, True, True, True, True, False, True]
+              }
 
     def __init__(self, pos, a=10, l=50, e=3):
         '''pos es la posicion del caracter, a y l son el ancho y largo de los segmentos, e es el espacio entre segmentos'''
@@ -93,8 +98,8 @@ class Caracter(pygame.Surface):
         self.pos = pos
         self.a = a
         self.l = l
-	    self.esp = e
-	    self.fondo = pygame.Color(0,0,0,0)  # Fondo transparente por defecto
+        self.esp = e
+        self.fondo = pygame.Color(0,0,0,0)  # Fondo transparente por defecto
 
         #   Segmentos
         #
@@ -136,30 +141,30 @@ class Caracter(pygame.Surface):
                           self.g
                          ]
 
-    	super(Caracter, self).__init__((l+a+2*e+1, 2*l+a+4*e+1), pygame.HWSURFACE|pygame.SRCALPHA)
+        super(Caracter, self).__init__((l+a+2*e+1, 2*l+a+4*e+1), pygame.HWSURFACE|pygame.SRCALPHA)
 
     def update(self):
 
         for s in self.segmentos:
             s.update()
-		
-	def render(self, val, sup):
-		'''Digito a renderizar'''
+        
+    def render(self, val, sup):
+        '''Digito a renderizar'''
 
 
-		segVis = Caracter.digitos[val]  # Segmentos visibles para el digito pasado
+        segVis = Caracter.digitos[val]  # Segmentos visibles para el digito pasado
 
-		self.fill(self.Fondo)
+        self.fill(self.fondo)
 
-		for indice in range(0,7):
-			self.segmentos[indice].visible = segVis[indice]
-			self.segmentos[indice].render(self)
-		
-		sup.blit(self, self.pos)
+        for indice in range(0,7):
+            self.segmentos[indice].visible = segVis[indice]
+            self.segmentos[indice].render(self)
+        
+        sup.blit(self, self.pos)
 
 
 class Reloj(pygame.Surface):
-	'''Reloj digital con display de 7 segmentos'''
+    '''Reloj digital con display de 7 segmentos'''
 
     def __init__(self, pos):
 
@@ -168,6 +173,7 @@ class Reloj(pygame.Surface):
         self.ancho = 66
         self.dosPuntos = 10
         self.esp = 5
+        self.fondo = pygame.Color(0,0,0,0)  # Transparente por defecto
         self.h1 = Caracter((0,0))
         self.h2 = Caracter((self.ancho + self.esp,0))
         self.p1 = Puntos(  (2*self.ancho + 2*self.esp,0))
@@ -179,12 +185,25 @@ class Reloj(pygame.Surface):
     
         self.hora = [self.h1, self.h2, self.p1, self.m1, self.m2, self.p2, self.s1, self.s2]
 
-    	super(Reloj, self).__init__((6*self.ancho + 7*self.esp + 2*self.dosPuntos, self.h1.get_height()), pygame.HWSURFACE|pygame.SRCALPHA)
+        super(Reloj, self).__init__((6*self.ancho + 7*self.esp + 2*self.dosPuntos, self.h1.get_height()), pygame.HWSURFACE|pygame.SRCALPHA)
+
+    # def get_size(self):
+    #     '''Devuelve el ancho y alto del reloj'''
+
+    #     ancho = 0
+    #     alto = 0
+    #     for car in self.hora:
+    #         ancho = ancho + car.get_width()
+    #         if car.get_height > alto:
+    #             alto = car.get_height
+
+    #     return (ancho, alto)
 
     def render(self, sup, strhora=""):
+        self.fill(self.fondo)
 
-    	if strhora == "":  # Si strhora esta vacio muestro la hora actual
-    		strhora = time.strftime('%H:%M:%S')
+        if strhora == "":  # Si strhora esta vacio muestro la hora actual
+            strhora = time.strftime('%H:%M:%S')
 
 
         for i in range(8):
@@ -195,3 +214,29 @@ class Reloj(pygame.Surface):
         sup.blit(self, self.pos)
 
 #        print valor
+def main(args):
+
+    pygame.init()
+#r = pygame.Rect((70,70),(170,150))
+
+    negro = pygame.Color(0,0,0)
+    reloj = Reloj((0,0))
+    ventana = pygame.display.set_mode(reloj.get_size())
+    
+
+    while True:
+        ventana.fill(negro)
+
+#        hora = time.strftime('%H:%M:%S')
+        reloj.render(ventana)
+
+        eventos()
+        pygame.display.update()
+
+    return 0
+
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
+
